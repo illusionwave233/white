7c4f6b5080065fceb9e76169a348eecfbf9cd1d5
illusionwave233 <amin.msh233@gmail.com>
2017-06-11 14:24:44 +0430

    ok

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from checker import Checker
 import configs
+import logging
 app = Checker()
 print("starting checking numbers from a range...")
 app.check_range(configs.area_code, configs.start_range, configs.end_range)
